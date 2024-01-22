b6bf556da223d7613ccb5edce9df53df51f5adea
JDWolffs <tentive22@gmail.com>
2024-01-22 14:15:46 +0200

    Basic programming begun

---
+++ script.js
@@ -0,0 +1,78 @@
+//body = document.querySelector(`body`);
+display = document.getElementById("display");
+
+zeroZero = document.getElementById("zeroZeroB");
+////let zeroZero = 00;
+zeroZero.addEventListener("click", () => {display.innerHTML += "00"});
+
+zero = document.getElementById("zeroB");
+//let zero = 0;
+zero.addEventListener("click", () => {display.innerHTML += "0"});
+
+one = document.getElementById("oneB");
+//let one = 1;
+one.addEventListener("click", () => {display.innerHTML += "1"});
+
+two = document.getElementById("twoB");
+//let two = 2;
+two.addEventListener("click", () => {display.innerHTML += "2"});
+
+three = document.getElementById("threeB");
+//let three = 3;
+three.addEventListener("click", () => {display.innerHTML += "3"});
+
+four = document.getElementById("fourB");
+//let four = 4;
+four.addEventListener("click", () => {display.innerHTML += "4"});
+
+five = document.getElementById("fiveB");
+//let five = 5;
+five.addEventListener("click", () => {display.innerHTML += "5"});
+
+six = document.getElementById("sixB");
+//let six = 6;
+six.addEventListener("click", () => {display.innerHTML += "6"});
+
+seven = document.getElementById("sevenB");
+//let seven = 7;
+seven.addEventListener("click", () => {display.innerHTML += "7"});
+
+eight = document.getElementById("eightB");
+//let eight = 8;
+eight.addEventListener("click", () => {display.innerHTML += "8"});
+
+nine = document.getElementById("nineB");
+//let nine = 9;
+nine.addEventListener("click", () => {display.innerHTML += "9"});
+
+peroid = document.getElementById("peroidB");
+//let peroid = ".";
+peroid.addEventListener("click", () => {display.innerHTML += "."});
+
+add = document.getElementById("addB");
+//let add = "+";
+add.addEventListener("click", () => {display.innerHTML += "+"});
+
+subtract = document.getElementById("subtractB");
+//let subtract = "-";
+subtract.addEventListener("click", () => {display.innerHTML += "-"});
+
+multiply = document.getElementById("multiplyB");
+//let multiply = "*";
+multiply.addEventListener("click", () => {display.innerHTML += "*"});
+
+divide = document.getElementById("divideB");
+//let divide = "/";
+divide.addEventListener("click", () => {display.innerHTML += "/"});
+
+clear = document.getElementById("clearB");
+//let clear = "c";
+clear.addEventListener("click", () => {display.innerHTML += "c"});
+
+backSpace = document.getElementById("backSpaceB");
+//let backSpace = "b";
+backSpace.addEventListener("click", () => {display.innerHTML += "b"});
+
+equals = document.getElementById("equalsB");
+//let equals = "=";
+equals.addEventListener("click", () => {display.innerHTML += "="});
