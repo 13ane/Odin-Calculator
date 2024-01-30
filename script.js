59408570821660993bd19cb6a56098683d2cc88e
JDWolffs <tentive22@gmail.com>
2024-01-30 13:09:40 +0200

    Coding modified

--- script.js
+++ script.js
@@ -1,78 +1,17 @@
-//body = document.querySelector(`body`);
-display = document.getElementById("display");
+body = document.querySelector("body");
 
-zeroZero = document.getElementById("zeroZeroB");
-////let zeroZero = 00;
-zeroZero.addEventListener("click", () => {display.innerHTML += "00"});
+btn = document.getElementById("buttons");
 
-zero = document.getElementById("zeroB");
-//let zero = 0;
-zero.addEventListener("click", () => {display.innerHTML += "0"});
+Array.from(document.getElementsByClassName("number")).forEach((num) => (num.addEventListener("click", (a) => {display.innerHTML += a.target.value})));
 
-one = document.getElementById("oneB");
-//let one = 1;
-one.addEventListener("click", () => {display.innerHTML += "1"});
+Array.from(document.getElementsByClassName("operator")).forEach((num) => (num.addEventListener("click", (a) => {display.innerHTML += "Do a thing!"})));
 
-two = document.getElementById("twoB");
-//let two = 2;
-two.addEventListener("click", () => {display.innerHTML += "2"});
+// btn.addEventListener("click",(a) => 
+//     {if (a.target.class = "number") 
+//         {display.innerHTML += a.target.value}
+//     else {display.innerHTML = "WhoopsieDoodle"}}
+// );
 
-three = document.getElementById("threeB");
-//let three = 3;
-three.addEventListener("click", () => {display.innerHTML += "3"});
-
-four = document.getElementById("fourB");
-//let four = 4;
-four.addEventListener("click", () => {display.innerHTML += "4"});
-
-five = document.getElementById("fiveB");
-//let five = 5;
-five.addEventListener("click", () => {display.innerHTML += "5"});
-
-six = document.getElementById("sixB");
-//let six = 6;
-six.addEventListener("click", () => {display.innerHTML += "6"});
-
-seven = document.getElementById("sevenB");
-//let seven = 7;
-seven.addEventListener("click", () => {display.innerHTML += "7"});
-
-eight = document.getElementById("eightB");
-//let eight = 8;
-eight.addEventListener("click", () => {display.innerHTML += "8"});
-
-nine = document.getElementById("nineB");
-//let nine = 9;
-nine.addEventListener("click", () => {display.innerHTML += "9"});
-
-peroid = document.getElementById("peroidB");
-//let peroid = ".";
-peroid.addEventListener("click", () => {display.innerHTML += "."});
-
-add = document.getElementById("addB");
-//let add = "+";
-add.addEventListener("click", () => {display.innerHTML += "+"});
-
-subtract = document.getElementById("subtractB");
-//let subtract = "-";
-subtract.addEventListener("click", () => {display.innerHTML += "-"});
-
-multiply = document.getElementById("multiplyB");
-//let multiply = "*";
-multiply.addEventListener("click", () => {display.innerHTML += "*"});
-
-divide = document.getElementById("divideB");
-//let divide = "/";
-divide.addEventListener("click", () => {display.innerHTML += "/"});
-
-clear = document.getElementById("clearB");
-//let clear = "c";
-clear.addEventListener("click", () => {display.innerHTML += "c"});
-
-backSpace = document.getElementById("backSpaceB");
-//let backSpace = "b";
-backSpace.addEventListener("click", () => {display.innerHTML += "b"});
-
-equals = document.getElementById("equalsB");
-//let equals = "=";
-equals.addEventListener("click", () => {display.innerHTML += "="});
+// if (btn.target.class = "number") {
+//     btn.addEventListener ("click", (a)=> {display.innerHTML += a.target.value})
+//}
