bb2251b319160cc0e253263664f91310bd2a7019
JDWolffs <tentive22@gmail.com>
2024-02-05 11:27:05 +0200

    Misunderstood instructions, starting over.

--- script.js
+++ script.js
@@ -4,14 +4,57 @@ btn = document.getElementById("buttons");
 
 Array.from(document.getElementsByClassName("number")).forEach((num) => (num.addEventListener("click", (a) => {display.innerHTML += a.target.value})));
 
-Array.from(document.getElementsByClassName("operator")).forEach((num) => (num.addEventListener("click", (a) => {display.innerHTML += "Do a thing!"})));
+// Array.from(document.getElementsByClassName("operator")).forEach((operator) => (operator.addEventListener("click", (a) => {display.innerHTML += "Do a thing!"})));
 
-// btn.addEventListener("click",(a) => 
-//     {if (a.target.class = "number") 
-//         {display.innerHTML += a.target.value}
-//     else {display.innerHTML = "WhoopsieDoodle"}}
-// );
+addition = document.getElementById("addB");
+let b = "waiting"
+addition.onclick = () => { if (a=NaN) {
+    a = display.innerHTML
+    display.innerHTML = ""
+    return a
+} else {
+    b = display.innerHTML
+    add()
+}}
 
-// if (btn.target.class = "number") {
-//     btn.addEventListener ("click", (a)=> {display.innerHTML += a.target.value})
-//}
+subtraction = document.getElementById("subtractB");
+subtraction.addEventListener("click", console.log("subtract"));
+
+multiplication = document.getElementById("multiplyB");  
+multiplication.addEventListener("click", console.log("multiply"));
+
+dividend = document.getElementById("divideB");
+dividend.addEventListener("click", console.log("divide"));
+
+clear = document.getElementById("clearB");
+clear.addEventListener("click", console.log("clear"));
+
+backspace = document.getElementById("backSpaceB");
+backspace.addEventListener("click", console.log("backspace"));
+
+equals = document.getElementById("equalsB");
+equals.addEventListener("click", console.log("equals"));
+
+function add(a, b) {
+    let c = a + b;
+    display.innerHTML = c;
+}
+
+function subtract(a, b) {
+    let c = a - b;
+    display.innerHTML = c;
+}
+
+function multiply(a, b) {
+    let c = a * b;
+    display.innerHTML = c;
+}
+
+function divide(a, b) {
+    let c = a / b;
+    display.innerHTML = c;
+}
+
+function clear (a) {
+    a = ""
+}
