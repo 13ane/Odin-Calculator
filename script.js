0c4f609977938472caff14b86ed02f8cf2d361bb
JDWolffs <tentive22@gmail.com>
2024-02-06 10:23:39 +0200

    Basic calculating function working!

--- script.js
+++ script.js
@@ -1,60 +1,74 @@
-body = document.querySelector("body");
+const body = document.querySelector("body");
 
-btn = document.getElementById("buttons");
+const btn = document.getElementById("buttons");
 
-Array.from(document.getElementsByClassName("number")).forEach((num) => (num.addEventListener("click", (a) => {display.innerHTML += a.target.value})));
+const numbers = Array.from(document.getElementsByClassName("number"));
 
-// Array.from(document.getElementsByClassName("operator")).forEach((operator) => (operator.addEventListener("click", (a) => {display.innerHTML += "Do a thing!"})));
+const operators = Array.from(document.getElementsByClassName("operator"));
 
-addition = document.getElementById("addB");
-let b = "waiting"
-addition.onclick = () => { if (a=NaN) {
-    a = display.innerHTML
+const formats =Array.from(document.getElementsByClassName("format"));
+
+document.getElementById("clearB").onclick = () => {
     display.innerHTML = ""
-    return a
-} else {
-    b = display.innerHTML
-    add()
+    rigger = "off";
+    first = "first";
+    operator = "operator";
+    second = "second";
+    third = "third";
+}
+
+numbers.forEach((num) => (num.addEventListener("click", (a) => {
+    if (trigger === "on") {
+        display.innerHTML = ""
+        trigger = "off"
+        display.innerHTML += a.target.value
+    } else {
+        display.innerHTML += a.target.value
+    };
+})));
+
+let trigger = "off";
+let first = "first";
+let operator = "operator";
+let second = "second";
+let third = "third";
+
+operators.forEach((ops) => (ops.addEventListener("click", (a) => {
+    if (first != "first") {
+        numSave(operator);
+        display.innerHTML = third
+        trigger = "on"
+        operator = a.target.value;
+    } else {
+        operator = a.target.value;
+        numSave(operator);
+        trigger = "on";
+};
+})))
+
+function  numSave (ops) {
+    if (first === "first") {
+        first = display.innerHTML
+    } else if (second === "second") {
+        second = display.innerHTML
+        third = calc(first, ops, second)
+    } else {
+        first = third
+        second = display.innerHTML
+        third = calc(first, ops, second)
 }}
 
-subtraction = document.getElementById("subtractB");
-subtraction.addEventListener("click", console.log("subtract"));
+function calc (first, ops, second) {
+    let operation = ""
+    if (ops === "+") {
+        operation = +first + +second
+    } else if (ops === "-") {
+        operation = +first - +second
+    } else if (ops === "*") {
+        operation = +first * +second
+    } else if (ops === "/") {
+        operation = +first / +second
+    }
 
-multiplication = document.getElementById("multiplyB");  
-multiplication.addEventListener("click", console.log("multiply"));
-
-dividend = document.getElementById("divideB");
-dividend.addEventListener("click", console.log("divide"));
-
-clear = document.getElementById("clearB");
-clear.addEventListener("click", console.log("clear"));
-
-backspace = document.getElementById("backSpaceB");
-backspace.addEventListener("click", console.log("backspace"));
-
-equals = document.getElementById("equalsB");
-equals.addEventListener("click", console.log("equals"));
-
-function add(a, b) {
-    let c = a + b;
-    display.innerHTML = c;
-}
-
-function subtract(a, b) {
-    let c = a - b;
-    display.innerHTML = c;
-}
-
-function multiply(a, b) {
-    let c = a * b;
-    display.innerHTML = c;
-}
-
-function divide(a, b) {
-    let c = a / b;
-    display.innerHTML = c;
-}
-
-function clear (a) {
-    a = ""
+    return operation
 }
