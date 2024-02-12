eb0bf3ff9cc27d13246d73cc1d6200757775148c
JDWolffs <tentive22@gmail.com>
2024-02-12 09:32:23 +0200

    One step forward, two steps back. Moving on.

--- script.js
+++ script.js
@@ -8,67 +8,106 @@ const operators = Array.from(document.getElementsByClassName("operator"));
 
 const formats =Array.from(document.getElementsByClassName("format"));
 
+//Switches for functions
+let triggerA = "off"; //Reset number input after operator is used
+let triggerB = "off";
+let first = "first";
+let operator = "operator";
+let second = "second";
+let third = "third";
+
+//AC Clear button reset
 document.getElementById("clearB").onclick = () => {
     display.innerHTML = ""
-    rigger = "off";
+    triggerA = "off";
     first = "first";
     operator = "operator";
     second = "second";
     third = "third";
-}
+};
 
+//Back Space button removes one character
+document.getElementById("backSpaceB").onclick = () => {
+    display.innerHTML = display.innerHTML.slice(0, -1);
+};
+
+//     document.getElementById("periodB").onclick = () => {
+
+// };
+
+//Displays results and repeats addition of second value
+// document.getElementById("equalsB").onclick = () => {
+//     display.innerHTML = second
+//     calc(first, operator, second)
+// }
+
+//Operates number buttons
 numbers.forEach((num) => (num.addEventListener("click", (a) => {
-    if (trigger === "on") {
+    if (triggerA === "on") {
         display.innerHTML = ""
-        trigger = "off"
+        triggerA = "off"
         display.innerHTML += a.target.value
     } else {
         display.innerHTML += a.target.value
     };
 })));
 
-let trigger = "off";
-let first = "first";
-let operator = "operator";
-let second = "second";
-let third = "third";
-
-operators.forEach((ops) => (ops.addEventListener("click", (a) => {
+//Operates operator buttons
+operators.forEach((operator) => (operator.addEventListener("click", (a) => {
     if (first != "first") {
         numSave(operator);
         display.innerHTML = third
-        trigger = "on"
-        operator = a.target.value;
+        triggerA = "on"
+        opChange(a.target.value)
     } else {
-        operator = a.target.value;
+        opChange(a.target.value)
         numSave(operator);
-        trigger = "on";
+        triggerA = "on";
 };
 })))
 
-function  numSave (ops) {
+//Records number entries
+function  numSave (operator) {
     if (first === "first") {
         first = display.innerHTML
     } else if (second === "second") {
         second = display.innerHTML
-        third = calc(first, ops, second)
+        third = calc(first, operator, second)
     } else {
         first = third
         second = display.innerHTML
-        third = calc(first, ops, second)
+        third = calc(first, operator, second)
 }}
 
-function calc (first, ops, second) {
+//calculates manually
+function calc (first, operator, second) {
     let operation = ""
-    if (ops === "+") {
+    if (operator === "+") {
         operation = +first + +second
-    } else if (ops === "-") {
+    } else if (operator === "-") {
         operation = +first - +second
-    } else if (ops === "*") {
+    } else if (operator === "*") {
         operation = +first * +second
-    } else if (ops === "/") {
+    } else if (operator === "/" && second != 0) {
         operation = +first / +second
+    } else {
+        operation = 666
+        triggerA = "off";
+        first = "first";
+        operator = "operator";
+        second = "second";
+        third = "third";
     }
-
     return operation
+}
+
+function opChange (a) {
+    operator = a
+}
+
+function cl () {
+    console.log(first)
+    console.log(second)
+    console.log(operator)
+    console.log(third)
 }
